e143a4b0dcb1eb87623da0fd87c5d02e28a0304b
Denis Zou <deniszou@gmail.com>
2020-02-25 18:42:07 -0500

    finished client side

--- client.py
+++ client.py
@@ -1,4 +1,7 @@
 import socket
+import threading
+import time
+
 
 def connectTs():
     try:
@@ -14,13 +17,14 @@ def connectTs():
     tcs.connect(server_binding)
     return tcs
 
+
 def client():
     try:
         rcs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[C]: Client socket created")
     except socket.error as err:
         print('socket open error: {} \n'.format(err))
-        #exit()
+        # exit()
 
     # Define the port on which you want to connect to the server
     port = 50007
@@ -30,10 +34,47 @@ def client():
     server_binding = (localhost_addr, port)
     rcs.connect(server_binding)
 
-    # Receive data from the server
-    data_from_server = rcs.recv(100)
-    print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
+    with open('PROJI-HNS.txt') as f:
+        lines = f.readlines()
 
-    # close the client socket
+    print(lines)
+
+    f = open("RESOLVED.txt", "w")
+    for line in lines:
+        rcs.send(line.strip("\n").encode('utf-8'))
+
+        time.sleep(1.5)
+
+        data_from_server = rcs.recv(1024)
+        d = data_from_server.decode('utf-8')
+        if d.endswith("A") or d.endswith("A\n"):
+            time.sleep(1.5)
+            print("[C]: Data matched in RS table and received from server:", d)
+            f.write(d)
+        elif d.endswith("N") or d.endswith("N\n"):
+            time.sleep(1.5)
+            print("[C]: No match found in RS table, data ends with NS", d)
+            connectTS = connectTs()
+
+            connectTS.send(line.strip("\n").encode('utf-8'))
+            time.sleep(1.5)
+
+            data_from_ts_server = connectTS.recv(1024)
+            m = data_from_ts_server.decode('utf-8')
+            if m.endswith("A") or m.endswith("A\n"):
+                print("[C]: Data matched in TS table and received from server", m)
+                f.write(m)
+            else:
+                print("[C]: Hostname - error: HOST NOT FOUND", m)
+                f.write(m)
+
+    f.close()
     rcs.close()
     exit()
+
+
+t2 = threading.Thread(name='client', target='client')
+t2.start()
+input("hit ENTER to exit")
+
+exit()
