7a7d63aa640ffeca9d51ff691f9be5b89811f4e3
Denis Zou <deniszou@gmail.com>
2020-02-25 18:59:55 -0500

    updated parameters

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import time
+import sys
 
 
 def connectTs():
@@ -9,7 +10,8 @@ def connectTs():
         print("[C]: Connect to TS")
     except socket.error as err:
         print('socket open error: {} \n'.format(err))
-    port = 50008
+    tsListenPort = int(sys.argv[3])
+    port = tsListenPort
     localhost_addr = socket.gethostbyname(socket.gethostname())
 
     # connect to the server on local machine
@@ -27,46 +29,47 @@ def client():
         # exit()
 
     # Define the port on which you want to connect to the server
-    port = 50007
+    rsListenPort = int(sys.argv[2])
+    port = rsListenPort
     localhost_addr = socket.gethostbyname(socket.gethostname())
 
     # connect to the server on local machine
     server_binding = (localhost_addr, port)
     rcs.connect(server_binding)
 
-    with open('PROJI-HNS.txt') as f:
-        lines = f.readlines()
-
-    print(lines)
+    hostname =  sys.argv[1]
+    print(hostname)
 
     f = open("RESOLVED.txt", "w")
-    for line in lines:
-        rcs.send(line.strip("\n").encode('utf-8'))
 
+    #send to RS
+    rcs.send(hostname.strip("\n").encode('utf-8'))
+
+    time.sleep(1.5)
+
+    data_from_server = rcs.recv(1024)
+    d = data_from_server.decode('utf-8')
+    if d.endswith("A") or d.endswith("A\n"):
+        time.sleep(1.5)
+        print("[C]: Data matched in RS table and received from server:", d)
+        f.write(d)
+    elif d.endswith("NS") or d.endswith("NS\n"):
+        time.sleep(1.5)
+        print("[C]: No match found in RS table, data ends with NS", d)
+    #send to TS
+        connectTS = connectTs()
+
+        connectTS.send(hostname.strip("\n").encode('utf-8'))
         time.sleep(1.5)
 
-        data_from_server = rcs.recv(1024)
-        d = data_from_server.decode('utf-8')
-        if d.endswith("A") or d.endswith("A\n"):
-            time.sleep(1.5)
-            print("[C]: Data matched in RS table and received from server:", d)
-            f.write(d)
-        elif d.endswith("N") or d.endswith("N\n"):
-            time.sleep(1.5)
-            print("[C]: No match found in RS table, data ends with NS", d)
-            connectTS = connectTs()
-
-            connectTS.send(line.strip("\n").encode('utf-8'))
-            time.sleep(1.5)
-
-            data_from_ts_server = connectTS.recv(1024)
-            m = data_from_ts_server.decode('utf-8')
-            if m.endswith("A") or m.endswith("A\n"):
-                print("[C]: Data matched in TS table and received from server", m)
-                f.write(m)
-            else:
-                print("[C]: Hostname - error: HOST NOT FOUND", m)
-                f.write(m)
+        data_from_ts_server = connectTS.recv(1024)
+        m = data_from_ts_server.decode('utf-8')
+        if m.endswith("A") or m.endswith("A\n"):
+            print("[C]: Data matched in TS table and received from server", m)
+            f.write(m)
+        else:
+            print("[C]: Hostname - error: HOST NOT FOUND", m)
+            f.write(m)
 
     f.close()
     rcs.close()
